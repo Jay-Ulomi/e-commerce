c1d02a724c9fcf19e0b50ea23a0e5c67c2714d89
Jay-Ulomi <71974270+Jay-Ulomi@users.noreply.github.com>
2024-01-20 08:46:12 +0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,21 @@ Route::get('/users', [UserController::class, 'index']);
 Route::post('/register', [UserController::class, 'register']);
 
 
-Route::get('/products_index', [ProductController::class, 'index'])->name('products_index');
+
 Route::post('/products', [ProductController::class, 'create']);
 
-// Authentication routes
-Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // User Activity Log routes
-Route::get('/user-activity-logs', [UserActivityLogController::class, 'index']);
+Route::get('/user-activity-logs/{user_id}', [UserActivityLogController::class, 'index']);
 
 Route::group(['middleware' => 'log.user.activity'], function () {
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::post('/products_purchase/{productId}', [ProductController::class, 'purchase'])->name('purchase');
+    Route::get('/products_index', [ProductController::class, 'index'])->name('products_index');
+
+    Route::post('/products_purchase/{productId}', [ProductController::class, 'purchase'])->name('products_purchase');
 
 });
 
